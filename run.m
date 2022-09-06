7fcb7b49fc95a238052d0029b3f116e59ea131a0
christinadanielphysics <53619458+christinadanielphysics@users.noreply.github.com>
2022-09-06 17:50:27 -0400

    tested apply function for creation operator

--- run.m
+++ run.m
@@ -1,3 +1,6 @@
-test_creation_operator = CreationOperator(1,"up");
-first_state = OrderedOccupationState(0,[],[]);
-test_creation_operator.apply(first_state);
+test_creation_operator = CreationOperator(2,"up");
+initial_up_spins = [1];
+initial_down_spins = [];
+first_state = OrderedOccupationState(1,initial_up_spins,initial_down_spins);
+second_state = test_creation_operator.apply(first_state)
+second_state.Up_Spins;
