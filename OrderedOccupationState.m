b6672544b29d9f6952d1ad8fc4d69531d5ec09eb
christinadanielphysics <53619458+christinadanielphysics@users.noreply.github.com>
2022-09-06 21:15:04 -0400

    annihilation operator code completed

--- OrderedOccupationState.m
+++ OrderedOccupationState.m
@@ -11,11 +11,5 @@ classdef OrderedOccupationState
             obj.Up_Spins = Up_Spins;
             obj.Down_Spins = Down_Spins;
         end
-
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
-        end
     end
 end
--- run.m
+++ run.m
@@ -1,6 +1,5 @@
-test_creation_operator = CreationOperator(2,"up");
-initial_up_spins = [1];
-initial_down_spins = [];
+test_annihilation_operator = AnnihilationOperator(3,"down");
+initial_up_spins = [1;3;5];
+initial_down_spins = [3;4];
 first_state = OrderedOccupationState(1,initial_up_spins,initial_down_spins);
-second_state = test_creation_operator.apply(first_state)
-second_state.Up_Spins;
+second_state = test_annihilation_operator.apply(first_state)
