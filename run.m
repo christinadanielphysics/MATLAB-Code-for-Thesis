f37ea4350efbc8e65dcd4831d2f1c9b4727102d5
christinadanielphysics <53619458+christinadanielphysics@users.noreply.github.com>
2022-09-10 23:42:39 -0400

    added l1 magic

--- run.m
+++ run.m
@@ -1,19 +1,27 @@
+%% Setup
+
 clc;
 clear;
 close all;
 
+%% Compressive Sensing Parameters
+
+start_time_inclusive = 0;
+end_time_inclusive = 50;
+number_of_time_values = 500;
+
 %% System
 
 Number_of_Spatial_Orbitals = 4;
-Number_of_Spin_Up_Electrons = 2; % must be >= 2 to compuate a nonzero spin-up lesser green's function 
-Number_of_Spin_Down_Electrons = 2; % must be >= 2 to compute a nonzero spin-down lesser green's function 
+Number_of_Spin_Up_Electrons = 2; % must be >= 2 to compuate a spin-up lesser green's function that is nonzero
+Number_of_Spin_Down_Electrons = 2; % must be >= 2 to compute a spin-down lesser green's function that is nonzero
 
 system = System(Number_of_Spatial_Orbitals,Number_of_Spin_Up_Electrons,Number_of_Spin_Down_Electrons,true,true);
 
 %% Hubbard
 
-U = 1;
-t = 1;
+U = 1.5;
+t = 1.5;
 connected_ends = true;
 
 system_minus_up = System(Number_of_Spatial_Orbitals,Number_of_Spin_Up_Electrons-1,Number_of_Spin_Down_Electrons,false,true);
@@ -29,7 +37,7 @@ spatial_orbital_index_j = 1;
 
 lesser_green = LesserGreen(spin,spatial_orbital_index_i,spatial_orbital_index_j,hubbard_model);
 
-t_values = 0:0.1:35;
+t_values = linspace(start_time_inclusive,end_time_inclusive,number_of_time_values);
 
 [lesser_real,lesser_imaginary] = lesser_green.compute(t_values);
 
@@ -42,12 +50,14 @@ greater_green = GreaterGreen(spin,spatial_orbital_index_i,spatial_orbital_index_
 %% Plotting
 
 figure;
-plot(t_values,greater_imaginary,t_values,greater_real)
-title('Greater')
-
-figure;
-plot(t_values,lesser_imaginary,t_values,lesser_real)
-title('Lesser')
+plot(t_values,greater_imaginary,'cyan')
+hold on;
+plot(t_values,greater_real,'red')
+hold on;
+scatter(t_values,lesser_imaginary,'green')
+hold on;
+scatter(t_values,lesser_real,'blue')
+title('Lesser and Greater')
 
 figure;
 scatter(greater_green.angular_frequency_differences,greater_green.weights);
